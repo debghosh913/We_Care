9f0197721c35be6f732718c1006d835e4fd102cc
Yeeshaj Aarshin Srivastava <yeeshaj.srivastava@gmail.com>
2021-09-24 20:51:44 +0530

    Add Bean Class
    Co-Authored-By: Samarthi <50495591+Samarthi@users.noreply.github.com>
    Co-Authored-By: Yatharth1515 <91327197+Yatharth1515@users.noreply.github.com>
    Co-Authored-By: debghosh913 <71193327+debghosh913@users.noreply.github.com>

---
+++ We Care/src/main/java/Beans/User.java
@@ -0,0 +1,72 @@
+package Beans;
+
+public class User {
+
+	String user_id,name,country,state,city,address,area,phone_no;
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getPhone_no() {
+		return phone_no;
+	}
+
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+	
+	
+}
